eb5bf1eb76114fd78c65e18d39df859277eef886
Vladimir <slight1989@gmail.com>
2022-09-11 12:35:30 +0400

    add style to blocks in index

--- src/app.js
+++ src/app.js
@@ -2,29 +2,50 @@
 // const compiledFunction = pug.compileFile('index.pug');
 // pug.renderFile('template.pug');
 
+// import Swiper JS
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper styles
 
-  // import Swiper JS
-  import Swiper, { Navigation, Pagination } from 'swiper';
-  // import Swiper styles
-  
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-  const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Pagination],
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+const gearType = document.getElementById('gearType');
+const bodyType = document.getElementById('bodyType');
+
+const optionsContainer = document.querySelectorAll('.options-container');
+const optionsList = document.querySelectorAll('.option');
+
+optionsList.forEach((i) => {
+  i.addEventListener('click', () => {
+    console.log(i);
+    selected.innerHTML = i.querySelector('label').innerHTML;
+    optionsContainer.forEach((el) => {
+      el.addEventListener('onClick', () => {});
+    });
   });
+});
+
+function toggleSelect(item) {
+  console.log(item);
+  item.addEventListener('click', () => {
+    console.log(item.querySelector('.options-container'));
+  });
+  // optionsContainer.classList.toggle('active');
+}
+toggleSelect(gearType);
+toggleSelect(bodyType);
