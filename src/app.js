6deb09012f75577020dbb6eed00141145909e267
Vladimir <slight1989@gmail.com>
2022-09-12 22:54:38 +0400

    fix style buttons, links, input

--- src/app.js
+++ src/app.js
@@ -1,51 +1,51 @@
-// const pug = require('pug');
-// const compiledFunction = pug.compileFile('index.pug');
-// pug.renderFile('template.pug');
+// // const pug = require('pug');
+// // const compiledFunction = pug.compileFile('index.pug');
+// // pug.renderFile('template.pug');
 
-// import Swiper JS
-import Swiper, { Navigation, Pagination } from 'swiper';
-// import Swiper styles
+// // import Swiper JS
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// // import Swiper styles
 
-const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Pagination],
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+// const swiper = new Swiper('.swiper', {
+//   modules: [Navigation, Pagination],
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-  },
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//     type: 'bullets',
+//   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-const gearType = document.getElementById('gearType');
-const bodyType = document.getElementById('bodyType');
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
+// const gearType = document.getElementById('gearType');
+// const bodyType = document.getElementById('bodyType');
 
-const optionsContainer = document.querySelectorAll('.options-container');
-const optionsList = document.querySelectorAll('.option');
+// const optionsContainer = document.querySelectorAll('.options-container');
+// const optionsList = document.querySelectorAll('.option');
 
-optionsList.forEach((i) => {
-  i.addEventListener('click', () => {
-    console.log(i);
-    selected.innerHTML = i.querySelector('label').innerHTML;
-    optionsContainer.forEach((el) => {
-      el.addEventListener('onClick', () => {});
-    });
-  });
-});
+// optionsList.forEach((i) => {
+//   i.addEventListener('click', () => {
+//     console.log(i);
+//     selected.innerHTML = i.querySelector('label').innerHTML;
+//     optionsContainer.forEach((el) => {
+//       el.addEventListener('onClick', () => {});
+//     });
+//   });
+// });
 
-function toggleSelect(item) {
-  console.log(item);
-  item.addEventListener('click', () => {
-    console.log(item.querySelector('.options-container'));
-  });
-  // optionsContainer.classList.toggle('active');
-}
-toggleSelect(gearType);
-toggleSelect(bodyType);
+// function toggleSelect(item) {
+//   console.log(item);
+//   item.addEventListener('click', () => {
+//     console.log(item.querySelector('.options-container'));
+//   });
+//   // optionsContainer.classList.toggle('active');
+// }
+// toggleSelect(gearType);
+// toggleSelect(bodyType);
